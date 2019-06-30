1caf9c842f187d928a5978b385f13a688fd5dacb
billsjchw <billsjc@126.com>
2019-07-01 00:13:57 +0800

    Two Players

--- code/QSnake/qsnake.cpp
+++ code/QSnake/qsnake.cpp
@@ -20,11 +20,16 @@ const QPointF QSnake::POLY_FOOD[QSnake::POLY_FOOD_N] = {
 const QColor QSnake::COLOUR_BACKGROUND(157, 164, 95);
 const QColor QSnake::COLOUR_ITEM(76, 74, 12);
 const QColor QSnake::COLOUR_ALPHA_BLACK(0, 0, 0, 90);
+const QColor QSnake::COLOUR_RED(211, 11, 55);
+const QColor QSnake::COLOUR_BLUE(0, 69, 155);
 const QString QSnake::ICON_START(":/img/start.png");
 const QImage QSnake::ICON_CUP_WHITE(":/img/cup_white.png");
 const QImage QSnake::ICON_CUP_ITEM(":/img/cup_item.png");
 const QString QSnake::ICON_RESTART(":/img/restart.png");
 const QString QSnake::ICON_SETTING(":/img/setting.png");
+const QString QSnake::ICON_FINISH(":/img/finish.png");
 const QImage QSnake::IMG_GAME_OVER(":/img/game_over.png");
-const QFont QSnake::FONT_BRITANIC("Britannic Bold", 18);
+const QFont QSnake::FONT_BRITANIC("Britannic Bold", 22);
 const QFont QSnake::FONT_BRITANIC_LARGE("Britannic Bold", 30);
+const QFont QSnake::FONT_YAHEI("Microsoft Yahei", 12);
+const QFont QSnake::FONT_YAHEI_LARGE("Microsoft Yahei", 40);
--- code/QSnake/qsnake.h
+++ code/QSnake/qsnake.h
@@ -2,11 +2,12 @@
 #define QSNAKE_H
 
 #include <Qt>
-#include <QObject>
 #include <QWidget>
 #include <QPainter>
 #include <QtEvents>
 #include <QPushButton>
+#include <QButtonGroup>
+#include <QRadioButton>
 #include <QIcon>
 #include <QFont>
 #include <QString>
@@ -16,6 +17,7 @@
 #include <QRect>
 #include <QColor>
 #include <QPen>
+#include <QSize>
 #include <deque>
 #include <stack>
 #include <queue>
@@ -27,27 +29,39 @@
 class QSnake: public QWidget {
 Q_OBJECT
 private:
-    enum State {WELCOME, PLAYING, GAMEOVER, SETTING, PAUSE};
+    enum State {WELCOME, SINGLE, DOUBLE, GAMEOVER_SINGLE, GAMEOVER_DOUBLE, SETTING, PAUSE};
     enum Direction {LEFT, UP, DOWN, RIGHT};
     struct Snake {
         static const int INIT_LEN = 3;
-        int len;
+        static const int INIT_INTV = 200;
+        static const int MIN_INTV = 80;
+        int len, interval;
         Direction dir;
         std::deque<QPoint> shape;
         Snake() {}
-        Snake(int m, int n): len(INIT_LEN) {
+        Snake(int m, int n): len(INIT_LEN), interval(INIT_INTV) {
             QPoint head(m / 2, n / 2);
             dir = Direction(rand() % 4);
             for (int i = INIT_LEN - 1; i >= 0; --i)
                 shape.push_back(head + OFFSET[3 - dir] * i);
         }
+        Snake(int m, int n, int k): len(INIT_LEN), interval(INIT_INTV) {
+            QPoint head(m / 3 * k, n / 2);
+            dir = Direction(rand() % 4);
+            for (int i = INIT_LEN - 1; i >= 0; --i)
+                shape.push_back(head + OFFSET[3 - dir] * i);
+
+        }
     };
+    enum Name {KNUTH, LINUS};
 private:
     static const int WIDTH_MAIN = 800;
+    static const int WIDTH_MAIN_DOUBLE = 1200;
     static const int HEIGHT_MAIN = 900;
     static const int HEIGHT_PG = 800;
     static const int ITEM_SIZE = 40;
     static const int M = WIDTH_MAIN / ITEM_SIZE;
+    static const int M_DOUBLE = WIDTH_MAIN_DOUBLE / ITEM_SIZE;
     static const int N = HEIGHT_PG / ITEM_SIZE;
     static const QPoint OFFSET[4];
     static const int POLY_SNAKE_N = 8;
@@ -57,36 +71,51 @@ private:
     static const QColor COLOUR_BACKGROUND;
     static const QColor COLOUR_ITEM;
     static const QColor COLOUR_ALPHA_BLACK;
+    static const QColor COLOUR_BLUE;
+    static const QColor COLOUR_RED;
     static const QString ICON_START;
     static const QImage ICON_CUP_WHITE;
     static const QImage ICON_CUP_ITEM;
     static const QString ICON_RESTART;
     static const QString ICON_SETTING;
+    static const QString ICON_FINISH;
     static const QImage IMG_GAME_OVER;
     static const QFont FONT_BRITANIC;
     static const QFont FONT_BRITANIC_LARGE;
+    static const QFont FONT_YAHEI;
+    static const QFont FONT_YAHEI_LARGE;
 private:
     std::stack<State> stk;
 private:
-    Snake knuth;
-    int ocpy[M][N];
-    std::queue<Direction> q;
+    State pnum;
+private:
+    Snake knuth, linus;
+    int ocpy[M_DOUBLE][N];
+    std::queue<Direction> q[2];
     QPoint food;
-    int tid, hi, interval;
+    int tid[2], hi;
 private:
     QPushButton * btn_start;
     QPushButton * btn_restart;
     QPushButton * btn_setting;
+    QPushButton * btn_finish;
+    QButtonGroup * btg_pnum;
+    QRadioButton * btn_single;
+    QRadioButton * btn_double;
 private:
+    void resize_fixed(int width, int height) {
+        setFixedSize(QWIDGETSIZE_MAX, QWIDGETSIZE_MAX);
+        resize(width, height);
+        setFixedSize(size());
+    }
     void create_button(State state) {
         if (state == WELCOME) {
             btn_start = new QPushButton(QIcon((ICON_START)), "", this);
             btn_start->setGeometry(280, 330, 240, 240);
             btn_start->setFlat(true);
             btn_start->setIconSize(QSize(240, 240));
-            QObject::connect(btn_start, SIGNAL(clicked()), this, SLOT(start()));
-            btn_start->show();
-        } else if (state == GAMEOVER) {
+            connect(btn_start, SIGNAL(clicked()), this, SLOT(start()));
+        } else if (state == GAMEOVER_SINGLE || state == GAMEOVER_DOUBLE) {
             btn_restart = new QPushButton(QIcon(ICON_RESTART), "", this);
             btn_setting = new QPushButton(QIcon(ICON_SETTING), "", this);
             btn_restart->setGeometry(250, 550, 120, 120);
@@ -95,73 +124,142 @@ private:
             btn_setting->setGeometry(430, 550, 120, 120);
             btn_setting->setFlat(true);
             btn_setting->setIconSize(QSize(120, 120));
-            QObject::connect(btn_restart, SIGNAL(clicked()), SLOT(restart()));
-            QObject::connect(btn_setting, SIGNAL(clicked()), SLOT(setting()));
-            btn_restart->show();
-            btn_setting->show();
-        } else if (state == PLAYING)
-            ;
+            connect(btn_restart, SIGNAL(clicked()), SLOT(restart()));
+            connect(btn_setting, SIGNAL(clicked()), SLOT(setting()));
+        } else if (state == SETTING) {
+            btn_single = new QRadioButton("1", this);
+            btn_double = new QRadioButton("2", this);
+            btn_single->setFont(FONT_YAHEI);
+            btn_double->setFont(FONT_YAHEI);
+            if (pnum == SINGLE)
+                btn_single->setChecked(true);
+            else
+                btn_double->setChecked(true);
+            btn_single->setGeometry(400, 270, 50, 50);
+            btn_double->setGeometry(500, 270, 50, 50);
+            btg_pnum = new QButtonGroup(this);
+            btg_pnum->addButton(btn_single, SINGLE);
+            btg_pnum->addButton(btn_double, DOUBLE);
+            btn_finish = new QPushButton(QIcon(ICON_FINISH), "", this);
+            btn_finish->setGeometry(340, 550, 120, 120);
+            btn_finish->setFlat(true);
+            btn_finish->setIconSize(QSize(120, 120));
+            connect(btn_finish, SIGNAL(clicked()), SLOT(finish_setting()));
+        }
+        show_button(state);
     }
     void destory_button(State state) {
         if (state == WELCOME)
             delete btn_start;
-        else if (state == GAMEOVER) {
+        else if (state == GAMEOVER_SINGLE || state == GAMEOVER_DOUBLE) {
             delete btn_restart;
             delete btn_setting;
+        } else if (state == SETTING) {
+            delete btg_pnum;
+            delete btn_single;
+            delete btn_double;
+            delete btn_finish;
+        }
+    }
+    void hide_button(State state) {
+        if (state == GAMEOVER_SINGLE || state == GAMEOVER_DOUBLE) {
+            btn_restart->hide();
+            btn_setting->hide();
+        }
+    }
+    void show_button(State state) {
+        if (state == WELCOME)
+            btn_start->show();
+        else if (state == GAMEOVER_SINGLE || state == GAMEOVER_DOUBLE) {
+            btn_restart->show();
+            btn_setting->show();
+        } else if (state == SETTING) {
+            btn_single->show();
+            btn_double->show();
+            btn_finish->show();
         }
     }
     bool valid(QPoint pos) {
-        return pos.x() >= 0 && pos.x() < M && pos.y() >= 0 && pos.y() < N;
+        int m = pnum == SINGLE ? M : M_DOUBLE;
+        return pos.x() >= 0 && pos.x() < m && pos.y() >= 0 && pos.y() < N
+                && !ocpy[pos.x()][pos.y()];
     }
     QPoint rnd_food() {
         QPoint ret(0, 0);
-        int k = rand() % (M * N - knuth.len - 1);
-        for (int i = 0; i < k; ++i) {
+        int k;
+        if (pnum == SINGLE)
+            k = rand() % (M * N - knuth.len - 1);
+        else
+            k = rand() % (M_DOUBLE * N - knuth.len - linus.len - 1);
+        for (int i = 0; i < k; ++i)
             do {
                 ret += QPoint(0, 1);
                 if (ret.y() >= N)
                     ret = QPoint(ret.x() + 1, 0);
             } while (ocpy[ret.x()][ret.y()]);
-        }
         return ret;
     }
     void init_game() {
-        knuth = Snake(M, N);
+        if (pnum == SINGLE)
+            knuth = Snake(M, N);
+        else {
+            knuth = Snake(M_DOUBLE, N, 1);
+            linus = Snake(M_DOUBLE, N, 2);
+        }
         memset(ocpy, 0, sizeof(ocpy));
         for (int i = 0; i < knuth.len; ++i)
             ocpy[knuth.shape[i].x()][knuth.shape[i].y()] = 1;
-        while (!q.empty())
-            q.pop();
+        while (!q[KNUTH].empty())
+            q[KNUTH].pop();
+        if (pnum == DOUBLE) {
+            for (int i = 0; i < linus.len; ++i)
+                ocpy[linus.shape[i].x()][linus.shape[i].y()] = 1;
+            while (!q[LINUS].empty())
+                q[LINUS].pop();
+        }
         food = rnd_food();
-        interval = 200;
-        tid = startTimer(interval);
+        if (pnum == DOUBLE)
+            resize_fixed(WIDTH_MAIN_DOUBLE, HEIGHT_MAIN);
+        update();
+        tid[KNUTH] = startTimer(knuth.interval);
+        if (pnum == DOUBLE)
+            tid[LINUS] = startTimer(linus.interval);
     }
 private slots:
     void start() {
-        stk.pop();
         destory_button(WELCOME);
-        stk.push(PLAYING);
-        create_button(PLAYING);
+        stk.pop();
+        stk.push(pnum);
+        create_button(pnum);
         init_game();
     }
     void restart() {
+        destory_button(stk.top());
         stk.pop();
-        destory_button(GAMEOVER);
-        stk.push(PLAYING);
-        create_button(PLAYING);
+        stk.push(pnum);
+        create_button(pnum);
         init_game();
     }
     void setting() {
+        hide_button(stk.top());
+        stk.push(SETTING);
+        create_button(SETTING);
+        update();
+    }
+    void finish_setting() {
+        pnum = State(btg_pnum->checkedId());
+        destory_button(SETTING);
+        stk.pop();
+        show_button(stk.top());
+        update();
     }
 public:
-    QSnake() {
+    QSnake(): pnum(SINGLE), hi(0) {
         setWindowTitle("QSnake");
-        setGeometry(0, 0, WIDTH_MAIN, HEIGHT_MAIN);
-        setFixedSize(size());
+        resize_fixed(WIDTH_MAIN, HEIGHT_MAIN);
         srand(unsigned(time(nullptr)));
-        hi = 0;
-        stk.push(WELCOME);
         create_button(WELCOME);
+        stk.push(WELCOME);
     }
     ~QSnake() {
         while (!stk.empty()) {
@@ -174,11 +272,11 @@ private:
         QPainter pt(this);
         pt.setPen(Qt::NoPen);
         pt.setBrush(COLOUR_BACKGROUND);
-        pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN);
         if (stk.top() == WELCOME) {
-            ;
+            pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN);;
         }
-        else if (stk.top() == PLAYING) {
+        else if (stk.top() == SINGLE) {
+            pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN);
             pt.setBrush(COLOUR_ITEM);
             pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN - HEIGHT_PG);
             pt.save();
@@ -207,8 +305,9 @@ private:
             }
             pt.translate(food);
             pt.drawConvexPolygon(POLY_FOOD, POLY_FOOD_N);
-        } else if (stk.top() == GAMEOVER) {
-            pt.drawImage(QRect(200, 50, 400, 400), IMG_GAME_OVER);
+        } else if (stk.top() == GAMEOVER_SINGLE) {
+            pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN);
+            pt.drawImage(QRect(204, 50, 400, 400), IMG_GAME_OVER);
             pt.save();
             pt.setBrush(COLOUR_ITEM);
             pt.translate(300, 360);
@@ -223,62 +322,135 @@ private:
             pt.drawText(400, 415, sc);
             QString hs = QString("%1").arg(hi, 3, 10, QChar('0'));
             pt.drawText(400, 495, hs);
+        } else if (stk.top() == GAMEOVER_DOUBLE) {
+            pt.drawRect(0, 0, WIDTH_MAIN, HEIGHT_MAIN);
+            pt.drawImage(QRect(204, 50, 400, 400), IMG_GAME_OVER);
+            pt.setBrush(COLOUR_RED);
+            pt.drawEllipse(302, 367, 54, 54);
+            pt.setBrush(COLOUR_BLUE);
+            pt.drawEllipse(302, 450, 54, 54);
+            pt.setPen(COLOUR_ITEM);
+            pt.setFont(FONT_BRITANIC_LARGE);
+            QString ks = QString("%1")
+                    .arg(knuth.len - Snake::INIT_LEN, 3, 10, QChar('0'));
+            pt.drawText(400, 415, ks);
+            QString ls = QString("%1")
+                    .arg(linus.len - Snake::INIT_LEN, 3, 10, QChar('0'));
+            pt.drawText(400, 495, ls);
+        } else if (stk.top() == DOUBLE) {
+            pt.drawRect(0, 0, WIDTH_MAIN_DOUBLE, HEIGHT_MAIN);
+            pt.setBrush(COLOUR_RED);
+            pt.drawRect(0, 0, WIDTH_MAIN_DOUBLE / 2, HEIGHT_MAIN - HEIGHT_PG);
+            pt.setBrush(COLOUR_BLUE);
+            pt.drawRect(WIDTH_MAIN_DOUBLE / 2, 0, WIDTH_MAIN_DOUBLE / 2, HEIGHT_MAIN - HEIGHT_PG);
+            pt.save();
+            pt.setPen(Qt::white);
+            pt.setFont(FONT_BRITANIC);
+            QString ks = QString("%1")
+                    .arg(knuth.len - Snake::INIT_LEN, 3, 10, QChar('0'));
+            pt.drawText(30, 65, ks);
+            QString ls = QString("%1")
+                    .arg(linus.len - Snake::INIT_LEN, 3, 10, QChar('0'));
+            pt.drawText(1100, 65, ls);
+            pt.restore();
+            pt.translate(0, HEIGHT_MAIN - HEIGHT_PG);
+            pt.setBrush(COLOUR_RED);
+            pt.scale(ITEM_SIZE, ITEM_SIZE);
+            for (int i = 0; i < knuth.len; ++i) {
+                pt.save();
+                pt.translate(knuth.shape[i]);
+                pt.drawConvexPolygon(POLY_SNAKE, POLY_SNAKE_N);
+                pt.restore();
+            }
+            pt.setBrush(COLOUR_BLUE);
+            for (int i = 0; i < linus.len; ++i) {
+                pt.save();
+                pt.translate(linus.shape[i]);
+                pt.drawConvexPolygon(POLY_SNAKE, POLY_SNAKE_N);
+                pt.restore();
+            }
+            pt.setBrush(COLOUR_ITEM);
+            pt.translate(food);
+            pt.drawConvexPolygon(POLY_FOOD, POLY_FOOD_N);
+        } else if (stk.top() == SETTING) {
+            pt.drawRect(0, 0, WIDTH_MAIN_DOUBLE, HEIGHT_MAIN);
+            pt.setPen(Qt::black);
+            pt.setFont(FONT_YAHEI_LARGE);
+            pt.drawText(270, 200, "Setting");
+            pt.setFont(FONT_YAHEI);
+            pt.drawText(260, 303, "Players:");
         }
     }
     void timerEvent(QTimerEvent * ev) {
-        if (ev->timerId() != tid || stk.top() != PLAYING)
+        if (stk.top() != SINGLE && stk.top() != DOUBLE)
             return;
-        if (!q.empty()) {
-            knuth.dir = q.front();
-            q.pop();
+        Snake * snake;
+        Name name;
+        if (ev->timerId() == tid[KNUTH]) {
+            snake = &knuth;
+            name = KNUTH;
+        } else if (pnum == DOUBLE && ev->timerId() == tid[LINUS]) {
+            snake = &linus;
+            name = LINUS;
+        } else
+            return;
+        if (!q[name].empty()) {
+            snake->dir = q[name].front();
+            q[name].pop();
         }
-        if (knuth.shape.back() + OFFSET[knuth.dir] == food) {
-            knuth.shape.push_back(food);
-            ++knuth.len;
-            ocpy[food.x()][food.y()] = 1;
-            if (knuth.len - Snake::INIT_LEN> hi)
-                hi = knuth.len - Snake::INIT_LEN;
+        if (snake->shape.back() + OFFSET[snake->dir] == food) {
+            snake->shape.push_back(food);
+            ++snake->len;
+            if (pnum == SINGLE && snake->len - Snake::INIT_LEN> hi)
+                hi = snake->len - Snake::INIT_LEN;
             food = rnd_food();
-            if (interval > 80) {
-                interval -= 2;
-                killTimer(tid);
-                tid = startTimer(interval);
+            if (snake->interval > Snake::MIN_INTV) {
+                snake->interval -= 2;
+                killTimer(tid[name]);
+                tid[name] = startTimer(snake->interval);
             }
         } else {
-            knuth.shape.push_back(knuth.shape.back() + OFFSET[knuth.dir]);
-            bool crash = false;
-            for (int i = 0; i < knuth.len - 1; ++i)
-                if (knuth.shape.back() == knuth.shape[i])
-                    crash = true;
-            if (crash || !valid(knuth.shape.back())) {
-                killTimer(tid);
-                stk.pop();
-                destory_button(PLAYING);
-                stk.push(GAMEOVER);
-                create_button(GAMEOVER);
-                update();
-                return;
-            }
-            ocpy[knuth.shape.front().x()][knuth.shape.front().y()] = 0;
-            knuth.shape.pop_front();
-            ocpy[knuth.shape.back().x()][knuth.shape.back().y()] = 1;
+            snake->shape.push_back(snake->shape.back() + OFFSET[snake->dir]);
+            ocpy[snake->shape.front().x()][snake->shape.front().y()] = 0;
+            snake->shape.pop_front();
         }
+        if (!valid(snake->shape.back())) {
+            killTimer(tid[KNUTH]);
+            if (pnum == DOUBLE)
+                killTimer(tid[LINUS]);
+            stk.pop();
+            destory_button(pnum);
+            stk.push(pnum == SINGLE ? GAMEOVER_SINGLE : GAMEOVER_DOUBLE);
+            resize_fixed(WIDTH_MAIN, HEIGHT_MAIN);
+            create_button(pnum == SINGLE ? GAMEOVER_SINGLE : GAMEOVER_DOUBLE);
+        } else
+            ocpy[snake->shape.back().x()][snake->shape.back().y()] = 1;
         update();
     }
     void keyPressEvent(QKeyEvent * ev) {
-        if (stk.top() != PLAYING)
+        if (stk.top() != SINGLE && stk.top() != DOUBLE)
             return;
         int dir;
+        Snake * snake = &knuth;
+        Name name = KNUTH;
         switch (ev->key()) {
-            case Qt::Key_Left: case Qt::Key_A: dir = LEFT; break;
-            case Qt::Key_Up: case Qt::Key_W: dir = UP; break;
-            case Qt::Key_Down: case Qt::Key_S: dir = DOWN; break;
-            case Qt::Key_Right: case Qt::Key_D: dir = RIGHT; break;
+            case Qt::Key_A: snake = &linus; name = LINUS;
+            case Qt::Key_Left: dir = LEFT; break;
+            case Qt::Key_W: snake = &linus; name = LINUS;
+            case Qt::Key_Up: dir = UP; break;
+            case Qt::Key_S: snake = &linus; name = LINUS;
+            case Qt::Key_Down: dir = DOWN; break;
+            case Qt::Key_D: snake = &linus; name = LINUS;
+            case Qt::Key_Right: dir = RIGHT; break;
             default: dir = -1; break;
         }
-        Direction cdir = q.empty() ? knuth.dir : q.front();
+        if (pnum == SINGLE) {
+            snake = &knuth;
+            name = KNUTH;
+        }
+        Direction cdir = q[name].empty() ? snake->dir : q[name].back();
         if (dir != -1 && dir != cdir && dir != 3 - cdir)
-            q.push(Direction(dir));
+            q[name].push(Direction(dir));
     }
 };
 
